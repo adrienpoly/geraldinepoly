b72f3a68a89c3d0a1f5ef12015055fe637b82627
Adrien POLY <adrienpoly@gmail.com>
2023-04-06 23:24:53 +0200

    add crisp chat

---
+++ frontend/javascript/controllers/crisp-chat_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus";
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.initializeCrisp();
+
+    }, 1000);
+  }
+
+  initializeCrisp() {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "fa4a0e7f-2db1-464f-98ce-d0056f168353";
+    const d = document;
+    const s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = true;
+    d.getElementsByTagName("head")[0].appendChild(s);
+    setTimeout(() => {
+      const client = document.querySelector(".crisp-client")
+      client.id = "crisp-client";
+      client.setAttribute("data-turbo-permanent", "true")
+    }, 1000);
+  }
+}
