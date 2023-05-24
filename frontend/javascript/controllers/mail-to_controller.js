12eb28b5d95720a40a898759ff53dd76b62d48f8
Adrien POLY <adrienpoly@gmail.com>
2023-05-24 14:10:49 +0200

    add links

---
+++ frontend/javascript/controllers/mail-to_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus";
+
+// this is to prevent to expose directly the email address in the HTML
+export default class extends Controller {
+  static values = { email: String }
+  connect() {
+  }
+
+  openEmail() {
+    const link = document.createElement('a');
+    link.href = `mailto:${this.emailValue}`;
+    link.style.display = 'none';
+    this.element.appendChild(link);
+    link.click();
+    requestAnimationFrame(() => {
+      this.element.removeChild(link);
+    })
+  }
+}
