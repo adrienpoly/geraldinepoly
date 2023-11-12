740bdbf60a6759c1b93b0aca43b159ebcf1ad990
Adrien POLY <adrienpoly@gmail.com>
2023-11-12 23:05:38 +0100

    add rfs

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,34 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    "tailwindcss/nesting": {},
-    "postcss-mixins": {},
-    "postcss-color-mod-function": {
+    'tailwindcss/nesting': {},
+    'postcss-mixins': {},
+    'postcss-color-mod-function': {
       // Uncomment the following to import CSS variables for use in `color-mod`:
       // importFrom: "frontend/styles/variables.css"
     },
-    "postcss-flexbugs-fixes": {},
-    "postcss-preset-env": {
+    rfs: {
+      twoDimensional: false,
+      baseValue: 16,
+      unit: 'rem',
+      breakpoint: 1200,
+      breakpointUnit: 'px',
+      factor: 5,
+      safariIframeResizeBugFix: false
+    },
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
       autoprefixer: {
-        flexbox: "no-2009",
+        flexbox: 'no-2009'
       },
       stage: 2,
       features: {
-        "nesting-rules": true,
-        "custom-media-queries": true,
-      },
+        'nesting-rules': true,
+        'custom-media-queries': true
+      }
     },
     cssnano: {
-      preset: "default",
-    },
-  },
-};
+      preset: 'default'
+    }
+  }
+}
